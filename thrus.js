0003934bb1ae2c6da46b1bc135c538289b73425a
Richard Remer <rich.remer@zingle.me>
2017-05-21 16:13:19 -0700

    exports

---
+++ thrus.js
@@ -0,0 +1,12 @@
+module.exports = {
+    after: require("./lib/after"),
+    before: require("./lib/before"),
+    concat: require("./lib/concat"),
+    filter: require("./lib/filter"),
+    flow: require("./lib/flow"),
+    map: require("./lib/map"),
+    partial: require("./lib/partial"),
+    partialMap: require("./lib/partial-map"),
+    sink: require("./lib/sink"),
+    thru: require("./lib/thru")
+}
